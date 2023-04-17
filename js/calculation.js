f8ea4146f4a421e5d017e4cbf37315924a33b027
Florian <floriandoppler@icloud.com>
2023-04-17 13:15:38 +0200

    Work on bet service

--- js/calculation.js
+++ js/calculation.js
@@ -1,5 +1,5 @@
 function houseProfit(quote1, quote2) {
-  return Math.abs(1 / quote1 + 1 / quote2 - 1);
+  return Math.round(Math.abs(1 / quote1 + 1 / quote2 - 1) * 100000) / 1000;
 }
 
 function getValues() {
